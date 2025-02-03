050c715c83801b5e2c15d3f7fc0db4afc8b84876
ZxR-Darki <zxr.darkness@gmail.com>
2025-02-04 00:13:00 +0300

    add craft steel for anvil

--- src/main/java/net/mcreator/ancientruins/init/AncientRuinsModItems.java
+++ src/main/java/net/mcreator/ancientruins/init/AncientRuinsModItems.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.ancientruins.item.SteelitemItem;
 import net.mcreator.ancientruins.item.LiteriumOreItem;
 import net.mcreator.ancientruins.AncientRuinsMod;
 
@@ -22,6 +23,7 @@ public class AncientRuinsModItems {
 	public static final RegistryObject<Item> BLOCKES = block(AncientRuinsModBlocks.BLOCKES, AncientRuinsModTabs.TAB_DRIEVNIIE_RUINY);
 	public static final RegistryObject<Item> LITERUIM_ORE = block(AncientRuinsModBlocks.LITERUIM_ORE, AncientRuinsModTabs.TAB_DRIEVNIIE_RUINY);
 	public static final RegistryObject<Item> LITERIUM_ORE = REGISTRY.register("literium_ore", () -> new LiteriumOreItem());
+	public static final RegistryObject<Item> STEELITEM = REGISTRY.register("steelitem", () -> new SteelitemItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
--- src/main/java/net/mcreator/ancientruins/procedures/ScriptforAnvilProcedure.java
+++ src/main/java/net/mcreator/ancientruins/procedures/ScriptforAnvilProcedure.java
@@ -17,8 +17,35 @@ public class ScriptforAnvilProcedure {
 		if (entity == null)
 			return;
 		double diamonds = 0;
-		if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY).getItem() == AncientRuinsModItems.LITERIUM_ORE
-				.get() && (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getItem() == Items.RAW_IRON) {
+		if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY).getItem() == Items.COAL
+				&& (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getItem() == Items.RAW_IRON) {
+			if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get(0)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+			if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				((Slot) _slots.get(1)).remove(1);
+				_player.containerMenu.broadcastChanges();
+			}
+			if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+				ItemStack _setstack = new ItemStack(Blocks.REINFORCED_DEEPSLATE);
+				_setstack.setCount((int) (new Object() {
+					public int getAmount(int sltid) {
+						if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
+							ItemStack stack = ((Slot) _slots.get(sltid)).getItem();
+							if (stack != null)
+								return stack.getCount();
+						}
+						return 0;
+					}
+				}.getAmount(2) + 1));
+				((Slot) _slots.get(2)).set(_setstack);
+				_player.containerMenu.broadcastChanges();
+			}
+		}
+		if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY).getItem() == AncientRuinsModItems.STEELITEM.get()
+				&& (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY)
+						.getItem() == AncientRuinsModItems.LITERIUM_ORE.get()) {
 			if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
 				((Slot) _slots.get(0)).remove(1);
 				_player.containerMenu.broadcastChanges();
